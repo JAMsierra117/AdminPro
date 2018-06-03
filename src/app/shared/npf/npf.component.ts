aa6edcc893c68e42c284a24d0faeff2a3e465041
jarmSierra117 <jamsierra117@gmail.com>
2018-06-02 19:46:19 -0600

    Primer commit

---
+++ src/app/shared/npf/npf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-npf',
+  templateUrl: './npf.component.html',
+  styles: []
+})
+export class NpfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
